c65ae923a7aca835499e4d9274aeaf397e2250ac
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2022-10-27 16:19:23 +0200

    message plus facile à réutiliser

--- example.sh
+++ example.sh
@@ -4,5 +4,5 @@
 echo "Carrefour" `cat notes.md |grep "^### "|cut -d " " -f 2-|cut -d ":" -f 1|sed -n $1p`
 params=`cat notes.md |grep "^### "|cut -d ":" -f 2|sed -n $1p`
 cmd="examples/get-crossroad-schematization.py   --osm  -l --display-preview  -c $params"
-echo $cmd
+echo 'PYTHONPATH=$PWD' $cmd
 PYTHONPATH=$PWD $cmd
